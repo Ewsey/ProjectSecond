12169b0136752a954bbb86f51623143f1c45d633
Ewsey <112591234+Ewsey@users.noreply.github.com>
2022-12-11 23:50:51 +0300

    Update main.py
    Пока не запускать, реализовано подключение и сохранение пользователя, можно протестировать в https://t.me/TestTelegramForEvseybot

--- main.py
+++ main.py
@@ -1,24 +1,70 @@
-import pygame
-pygame.font.init()
-intr_font = pygame.font.SysFont('Arial', 100)
+import telebot
+import pickle
 
-W_WIDTH = 800
-W_HEIGHT = 600
+DBD = './'
+Matches = []
+HelloText = 'Привет! Это бот для игры в шахматы с друзьями. Ты только что зарегистрировался'
+HelpText = 'Чтобы играть с другом напиши его никнейм. Если он запускал этот бот я отправлю ему запрос'
+TryingConnectText = 'Отправляю запрос на подключение...'
 
-screen = pygame.display.set_mode((W_WIDTH, W_HEIGHT))
-pygame.display.set_caption('Chess')
+def load_users():
+    try:
+        with open(DBD + 'Users.pickle', 'rb') as file:
+            users = pickle.load(file)
+    except FileNotFoundError:
+        users = {}
+        with open(DBD + 'Users.pickle', 'wb') as file:
+            pickle.dump(users, file)
+    return users
 
-# Вывод приветствия
-intr = intr_font.render('Chess', True, (180, 180, 180))
-draw_x = W_WIDTH//2 - intr.get_width()//2
-draw_y = W_HEIGHT//2 - intr.get_height()//2 - 10
-screen.blit(intr, (draw_x, draw_y))
-pygame.display.update()
 
-while 1:
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.QUIT:
-            exit()
+def start_game(u1, u2, i1, i2):
+    print('starting game with', u1, u2)
 
-    pygame.display.update()
+
+token = '5866341366:AAF7gOQ30992B2zgP38ZYk1Vod-EM3i0XTA'
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    """ При подключении нового аккаунта добавляем его в users и приветствуем
+    """
+    global users
+    username = message.from_user.username.lower()
+    userid = message.chat.id
+    print(f'New user: {username} id: {userid}')
+    if username not in users:
+        bot.send_message(userid, HelloText)
+        bot.send_message(userid, HelpText)
+        users[username] = message.chat.id
+        with open(DBD + 'Users.pickle', 'wb') as file:
+            pickle.dump(users, file)
+
+
+@bot.message_handler(commands=['help'])
+def help_handler(message):
+    bot.send_message(message.chat.id, HelpText)
+
+
+@bot.message_handler(func=lambda message: message.text[0] == '@')
+def create_game(message):
+    global users, Matches
+    username1 = message.from_user.username.lower()
+    id1 = message.chat.id
+    username2 = message.text[1:].lower()
+    if (username2, username1) in Matches:
+        id2 = users[username2]
+        Matches.remove((username2, username1))
+        start_game(username1, username2, id1, id2)
+        return
+    if username2 in users.keys():
+        bot.send_message(id1, TryingConnectText)
+        id2 = users[username2]
+        bot.send_message(id2, f'К тебе пытается подключиться @{username1}\nОтправь его никнейм для старта игры с ним')
+        Matches.append((username1, username2))
+
+
+users = load_users()
+print(users)
+bot.infinity_polling()
